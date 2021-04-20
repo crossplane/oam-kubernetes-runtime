a8827b31cfe10dba9b25a64a70e42f40a45f2d9a
Xisheng Cai <xishengcai@XishengdeMacBook-Pro.local>
2021-04-20 16:51:40 +0800

    fix bug fix bug #327

--- pkg/controller/v1alpha2/applicationconfiguration/apply.go
+++ pkg/controller/v1alpha2/applicationconfiguration/apply.go
@@ -100,6 +100,9 @@ type workloads struct {
 }
 
 func (a *workloads) Apply(ctx context.Context, status []v1alpha2.WorkloadStatus, w []Workload, ao ...resource.ApplyOption) error {
+	if len(w) == 0 {
+		return nil
+	}
 	// they are all in the same namespace
 	var namespace = w[0].Workload.GetNamespace()
 	for _, wl := range w {
